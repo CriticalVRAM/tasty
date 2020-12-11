86753ea6bf77b24f3f74ceaad7d7b49d90d5c6ea
Nani-Sore-0 <urke.radule@gmail.com>
2020-12-11 18:41:19 +0100

    booking validation

--- docs/js/book.js
+++ docs/js/book.js
@@ -1,57 +1,81 @@
-let formIsValid = false;
+let formIsValid = [false, false, false];
 
 let fullName = document.querySelector("#fullName");
 let email = document.querySelector("#email");
 let phone = document.querySelector("#phone");
 
-$(document).ready(function () {
-  if (fullName.value) validateFullName();
-});
-
 fullName.addEventListener("blur", validateFullName);
+email.addEventListener("blur", validateEmail);
+phone.addEventListener("blur", validatePhone);
 
-function validateFullName() {
-  let fullNameTooltip = document.querySelector("#fullName-invalid");
-  let nameRegEx = /^[A-Z]\w* [A-Z]\w*/;
-  if (!nameRegEx.test(fullName.value)) {
-    validationTooltipToggle(fullName, fullNameTooltip, false);
-    formIsValid = false;
-  } else {
-    validationTooltipToggle(fullName, fullNameTooltip, true);
-    formIsValid = true;
-  }
-}
-
-function validationTooltipToggle(inputElement, tooltipElement, inputIsValid) {
+function validationCSSToggle(inputElement, tooltipElement, inputIsValid) {
   if (!inputIsValid) {
     inputElement.classList.add("is-invalid");
     inputElement.classList.remove("is-valid");
     if (tooltipElement.classList.contains("d-none")) tooltipElement.classList.remove("d-none");
   } else {
-    inputElement.classList.remove("is-invalid");
     inputElement.classList.add("is-valid");
+    inputElement.classList.remove("is-invalid");
     if (!tooltipElement.classList.contains("d-none")) tooltipElement.classList.add("d-none");
   }
 }
 
-// name email phone
-// select input
-// add eventlistenter blur with anonimus function that that checks the value
-// check with a regex
-// if it's invalid toggle tooltip and toggle is-invalid
-// else toggle is-valid
+///////////////////////////////////////////////////////////////////////////////
+//* If there is value in any of the input on load call valdation functions
+$(document).ready(function () {
+  //! Call valdation functions if they have any value
+  if (fullName.value) validateFullName();
+  if (email.value) validateEmail();
+  if (phone.value) validatePhone();
+});
 
-// have a "is valid" variable
-// when the button is clicked check if "is valid"
-// if yes show text
-// if no run invalid style functions
+///////////////////////////////////////////////////////////////////////////////
+//* Validation functions for each input
+function validateFullName() {
+  let fullNameTooltip = document.querySelector("#fullName-invalid");
+  let nameRegEx = /^[A-Z]\w* [A-Z]\w*/;
+  if (!nameRegEx.test(fullName.value)) {
+    validationCSSToggle(fullName, fullNameTooltip, false);
+    formIsValid[0] = false;
+  } else {
+    validationCSSToggle(fullName, fullNameTooltip, true);
+    formIsValid[0] = true;
+  }
+}
+function validateEmail() {
+  let emailTooltip = document.querySelector("#email-invalid");
+  let emailRegEx = /^\S*@.*\.\w*/;
+  if (!emailRegEx.test(email.value)) {
+    validationCSSToggle(email, emailTooltip, false);
+    formIsValid[1] = false;
+  } else {
+    validationCSSToggle(email, emailTooltip, true);
+    formIsValid[1] = true;
+  }
+}
+function validatePhone() {
+  let phoneTooltip = document.querySelector("#phone-invalid");
+  let phoneRegEx = /^\d{3}[\d -]{5,8}$/;
+  if (!phoneRegEx.test(phone.value)) {
+    validationCSSToggle(phone, phoneTooltip, false);
+    formIsValid[2] = false;
+  } else {
+    validationCSSToggle(phone, phoneTooltip, true);
+    formIsValid[2] = true;
+  }
+}
 
+///////////////////////////////////////////////////////////////////////////////
+//* Check if all inputs are valid when button is clicked
 let btn = document.querySelector("#form-button");
 let text = document.querySelector("#success-text");
 btn.addEventListener("click", validateForm);
 function validateForm() {
-  if (!formIsValid) {
+  if (formIsValid.includes(false)) {
+    //! Call valdation functions
     validateFullName();
+    validateEmail();
+    validatePhone();
     text.classList.add("d-none");
   } else {
     text.classList.remove("d-none");
